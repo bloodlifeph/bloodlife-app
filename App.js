c0916ff724893f612ad259a9470b7ce22617cf16
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-11-12 13:34:39 +0800

    changed type and location to Picker

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Styles from './styles'
-import { KeyboardAvoidingView, TouchableHighlight, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import { KeyboardAvoidingView, TouchableHighlight, StyleSheet, Text, TextInput, View, Image, Picker, Dimensions } from 'react-native';
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default class App extends React.Component {
 
@@ -11,7 +12,8 @@ export default class App extends React.Component {
       bloodType: 'Blood Type Needed',
       city: 'Nearest City',
       name: "Inquirer / Patient's name",
-      contactNumber: "Contact Number"
+      contactNumber: "Contact Number",
+      showPickers: true
     }
   }
 
@@ -24,24 +26,65 @@ export default class App extends React.Component {
       <KeyboardAvoidingView style={Styles.container} behavior="padding" >
         <Image style={Styles.logo} source={require('./assets/logo.png')} />
         <Text style={Styles.instructionText}>Please choose the blood type that you are looking for and provide your contact details. Your inquiry will be broadcasted to the kind hearted blood donors.</Text>
-        <View style={{ width: 310 }}>
+
+        {this.state.showPickers &&
+          <View style={{ flex: 1, flexDirection: 'row', width: SCREEN_WIDTH - 50 }}>
+            <View style={{ flex: 1, marginTop: -60 }}>
+              <Picker
+                mode="dropdown"
+                color='blue'
+                selectedValue={this.state.bloodType}
+                onValueChange={(itemValue, itemIndex) => this.setState({ bloodType: itemValue })}>
+                <Picker.Item label="O+" value="O+" />
+                <Picker.Item label="O-" value="O-" />
+                <Picker.Item label="A+" value="A+" />
+                <Picker.Item label="A-" value="A-" />
+                <Picker.Item label="B+" value="B+" />
+                <Picker.Item label="B-" value="B-" />
+                <Picker.Item label="AB+" value="AB+" />
+                <Picker.Item label="AB-" value="AB-" />
+                <Picker.Item label="ANY" value="ANY" />
+              </Picker>
+            </View>
+            <View style={{ flex: 2, marginTop: -60 }} >
+              <Picker
+                selectedValue={this.state.city}
+                onValueChange={(itemValue, itemIndex) => this.setState({ city: itemValue })}>
+
+                <Picker.Item label="Antipolo" value="Antipolo" />
+                <Picker.Item label="Agoo City" value="Agoo City" />
+                <Picker.Item label="Bacolod City" value="Bacolod City" />
+                <Picker.Item label="Baguio" value="Baguio" />
+                <Picker.Item label="Cagayan de Oro" value="Cagayan de Oro" />
+                <Picker.Item label="Caloocan" value="Caloocan" />
+                <Picker.Item label="Cebu City" value="Cebu City" />
+                <Picker.Item label="Dagupan City" value="Dagupan City" />
+                <Picker.Item label="Davao City" value="Davao City" />
+                <Picker.Item label="Lapu-lapu City" value="Lapu-lapu City" />
+                <Picker.Item label="Manila" value="Manila" />
+                <Picker.Item label="Pasig" value="Pasig" />
+                <Picker.Item label="Quezon City" value="Quezon City" />
+                <Picker.Item label="San Fernando City" value="San Fernando City" />
+                <Picker.Item label="Taguig" value="Taguig" />
+                <Picker.Item label="Tacloban" value="Tacloban City" />
+                <Picker.Item label="Tagbiliran" value="Tagbilaran City" />
+                <Picker.Item label="Zamboanga City" value="Zamboanga City" />
+              </Picker>
+            </View>
+          </View>
+
+        }
+
+
+        <View style={[Styles.containerFormText, { marginTop: 50 }]}>
+
           <TextInput style={Styles.textInput}
-            onChangeText={(bloodType) => {
-              this.setState({
-                bloodType: bloodType
-              })
+            onFocus={() => {
+              this.setState({ showPickers: false })
             }}
-            value={this.state.bloodType}
-          />
-          <TextInput style={Styles.textInput}
-            onChangeText={(city) => {
-              this.setState({
-                city: city
-              })
+            onBlur={() => {
+              this.setState({ showPickers: true })
             }}
-            value={this.state.city}
-          />
-          <TextInput style={Styles.textInput}
             onChangeText={(name) => {
               this.setState({
                 name: name
@@ -50,6 +93,12 @@ export default class App extends React.Component {
             value={this.state.name}
           />
           <TextInput style={Styles.textInput}
+            onFocus={() => {
+              this.setState({ showPickers: false })
+            }}
+            onBlur={() => {
+              this.setState({ showPickers: true })
+            }}
             onChangeText={(contactNumber) => {
               this.setState({
                 contactNumber: contactNumber
@@ -58,14 +107,16 @@ export default class App extends React.Component {
             value={this.state.contactNumber}
           />
         </View>
-        <View style={{ width: 310 }}>
-          <TouchableHighlight
-            style={Styles.submit}
-            onPress={() => this.sendInquiry(this.props)}
-            underlayColor='#ff9999'>
-            <Text style={[Styles.submitText]}>Inquire</Text>
-          </TouchableHighlight>
-        </View>
+        {this.state.showPickers &&
+          <View style={Styles.container}>
+            <TouchableHighlight
+              style={Styles.submit}
+              onPress={() => this.sendInquiry(this.props)}
+              underlayColor='#ff9999'>
+              <Text style={[Styles.submitText]}>Inquire</Text>
+            </TouchableHighlight>
+          </View>
+        }
       </KeyboardAvoidingView>
     );
   }
