5a4a35a60c65998d8ff6f37599efd69b82c796bb
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-11-14 21:23:08 +0800

    extracted cities into another js

---
+++ cities.js
@@ -0,0 +1,21 @@
+const Cities = [
+    "Antipolo",
+    "Agoo City",
+    "Bacolod City",
+    "Baguio",
+    "Cagayan de Oro",
+    "Caloocan",
+    "Cebu City",
+    "Dagupan City",
+    "Davao City",
+    "Lapu-lapu City",
+    "Manila",
+    "Pasig",
+    "Quezon City",
+    "San Fernando City",
+    "Taguig",
+    "Tacloban",
+    "Tagbiliran",
+    "Zamboanga City"
+]
+export default Cities;
