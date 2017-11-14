dd5c6d2ea621b33dd1eb8151cab8d91ed3c907a9
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-11-14 21:34:26 +0800

    minor cleanup

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default class App extends React.Component {
     this.setState({ contactNumber: contactNumber });
   }
 
-  hidePickers() {
+  hidePickers = () => {
     this.setState({ showPickers: false })
   }
 
